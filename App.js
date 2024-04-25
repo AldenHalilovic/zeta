d9c81c19ccb122f970374b61ba9f80b1b6dd2020
Alden <aldehalilovic@gmail.com>
2024-04-25 16:15:17 +0200

    bottom navigation added

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-export default function App() {
+function Home() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>Home</Text>
     </View>
   );
 }
 
+function Reorder() {
+  return (
+    <View>
+      <Text>Reorder</Text>
+    </View>
+  );
+}
+
+function Cart() {
+  return (
+    <View>
+      <Text>Cart</Text>
+    </View>
+  );
+}
+function Account() {
+  return (
+    <View>
+      <Text>Account</Text>
+    </View>
+  );
+}
+
+const App = () => {
+  const Tab = createBottomTabNavigator();
+  return (
+    <NavigationContainer style={styles.container}>
+      <Tab.Navigator>
+        <Tab.Screen name="HOME" component={Home} />
+        <Tab.Screen name="REORDER" component={Reorder} />
+        <Tab.Screen name="CART" component={Cart} />
+        <Tab.Screen name="ACCOUNT" component={Account} />
+      </Tab.Navigator>
+      <StatusBar style="auto" />
+    </NavigationContainer>
+  );
+};
+
+export default App;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
